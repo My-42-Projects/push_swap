095e1ec9c77c3c452c270adcc12c35d359ff7783
David Ulrich <dulrich@c4r2p8.42vienna.com>
2024-07-01 13:53:17 +0200

    working on atol func

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:30:08 by dulrich           #+#    #+#             */
-/*   Updated: 2024/07/01 10:04:30 by dulrich          ###   ########.fr       */
+/*   Updated: 2024/07/01 13:42:20 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@ void	push_swap(t_node **a, t_node **b)
 	
 }
 
-void	sort_five(t_node *a, t_node *b)
+void	sort_five(t_node **a, t_node **b)
 {
 	/* 
 	push two nodes to stack b
 	do sort_three
 	
 	 */
+	pb(a);
+	pb(a);
 }
 
 void	sort_three(t_node *stack)
@@ -55,49 +57,77 @@ void	sort_three(t_node *stack)
 	}
 }
 
-t_node	**clone_stack(t_node **a)
+int	is_sorted(t_node *stack)
 {
 	
 }
 
-void	find_index(t_node **a)
+t_node	**clone_stack(t_node *a)
+{
+	
+}
+
+void	find_index(t_node *a)
 {
 	t_node	**c;
 
 	c = NULL;
 	c = clone_stack(a);
 	quicksort(c);
+	
 }
 
 void	ft_error()
 {
-	
 	write(STDERR_FILENO, "Error\n", 6);
+	/* 
+	- Free everything (nodes and artificial argv if used)
+	- exit
+	 */
+	exit(EXIT_FAILURE);
 }
 
-char	**artificial_argv(char *str)
+void	init_stack(t_node **stack, char **argv, int flag)
 {
-	/* 
-	
-	 */
+	int		i;
+	long	nbr;
+
+	i = 0;
+	nbr = 0;
+	while (argv[i])
+	{
+		if (is_syntax_error(argv[i]))
+			ft_error();
+		if (is_duplicate(argv[i]))
+			ft_error();
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			ft_error();
+		init_node();
+		i++;
+	}
+	if (flag)
+		free_artificial_argv(argv);
 }
 
 int	main(int argc, char **argv)
 {
 	t_node	**a;
 	t_node	**b;
-	char	**art_argv;
+	int		flag;
 
 	a = NULL;
 	b = NULL;
+	flag = 0;
 	if (argc <= 2)
 	{
 		if (argc == 2)
-			art_argv = artificial_argv(argv[1]);
+			argv = ft_split(argv[1], ' ');
 		else
-			return (1);
+			exit (EXIT_FAILURE);
+		flag = 1;
 	}
-	init_nodes(a, argv, argc == 2);
+	init_stack(a, argv, flag);
 	find_index(a);
 	if (!is_sorted(a))
 	{
